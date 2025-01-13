08a2dc11530db089fe0137efbba6e00013d461b4
Alidiblade <barnabasdemuse@gmail.com>
2025-01-13 10:02:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349125042727"
+global.pairingNumber = "243900365665"
 
 
 
